77e5d9db2865f1ccef6451b2ca185dc9bc7c7936
webmagicinformatica <56603855+webmagicinformatica@users.noreply.github.com>
2020-06-06 16:45:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "${var.Access_key_ID}"
-  secret_key = "${var.Secret_access_key}"
+  access_key = "AKIAY7UO4T4X7RQ44WDO"
+  secret_key = "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"
 }
 
 resource "aws_instance" "ec2example" {
